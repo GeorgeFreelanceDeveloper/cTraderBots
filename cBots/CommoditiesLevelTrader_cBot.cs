747744a704449e863f56ac8f8fe03e7b71b2ee9d
George Quant Analyst <112611533+GeorgeQuantAnalyst@users.noreply.github.com>
2023-08-01 14:05:56 +0200

    CommoditiesLevelTrader_cBot: updated input and compute validator

--- cBots/CommoditiesLevelTrader_cBot.cs
+++ cBots/CommoditiesLevelTrader_cBot.cs
@@ -7,6 +7,7 @@ using cAlgo.API;
 using cAlgo.API.Collections;
 using cAlgo.API.Indicators;
 using cAlgo.API.Internals;
+using System.Text.RegularExpressions;
 
 namespace cAlgo.Robots
 {
@@ -36,6 +37,7 @@ namespace cAlgo.Robots
         [Parameter(DefaultValue = "")]
         public string ExpirationDateString {get; set;}
         
+        private Regex ExpirationDatePattern = new Regex(@"^\d{4}/\d{2}/\d{2}$");
         
         private readonly double PercentageBeforeEntry = 0.33;
         
@@ -61,6 +63,7 @@ namespace cAlgo.Robots
         
         private int PendingOrderId {get; set;}
         
+
         
         public enum TradeDirectionType
         {
@@ -71,6 +74,18 @@ namespace cAlgo.Robots
         protected override void OnStart()
         {
             Print("Start CommoditiesLevelTrader_cBot");
+            
+            var inputErrorMessages = ValidateInputs();
+            if(inputErrorMessages.Count > 0)
+            {
+                Print("Validation input parameter errors:");
+                foreach (var message in inputErrorMessages)
+                {
+                    Print(message);
+                }
+                Stop();
+                return;
+            }
             
             Move = EntryPrice - StopLossPrice;
             TakeProfitPrice = EntryPrice + Move;
@@ -88,11 +103,11 @@ namespace cAlgo.Robots
             Print(String.Format("Amount: {0} lots", Symbol.VolumeInUnitsToQuantity(Amount)));
             Print(String.Format("BeforeEntryPrice: {0}", BeforeEntryPrice));
             Print(String.Format("ExpirationDate: {0}", ExpirationDate));
-            
-            var errMessages = Validate();
+
+            var errMessages = ValidateComputeValues();
             if(errMessages.Count > 0)
             {
-                Print("Validation errors:");
+                Print("Validation compute values errors:");
                 foreach (var message in errMessages)
                 {
                     Print(message);
@@ -138,7 +153,6 @@ namespace cAlgo.Robots
                 if(ReachBeforeEntryPriceTimestamp.Subtract(ReachBeforeEntryPriceTimestamp).TotalMinutes < PlaceTradeDelayInMinutes)
                 {
                     Print("Most fast movement to level, order do not place on exchange.");
-                    //enablePlaceTrade = false;
                     Stop();
                     return;
                 }
@@ -238,56 +252,69 @@ namespace cAlgo.Robots
             return false;
         }
         
-        private ArrayList Validate()
+        private ArrayList ValidateInputs()
         {
-            var err_messages = new ArrayList();
+            var errMessages = new ArrayList();
             
-            if (EntryPrice < 0)
+            if (EntryPrice <= 0)
             {
-                err_messages.Add(String.Format("WARNING: EntryPrice must be greater than 0. [EntryPrice: {0}]", EntryPrice));
+                errMessages.Add(String.Format("WARNING: EntryPrice must be greater than 0. [EntryPrice: {0}]", EntryPrice));
             }
             
-            if (StopLossPrice < 0)
+            if (StopLossPrice <= 0)
             {
-                err_messages.Add(String.Format("WARNING: StopLossPrice must be greater than 0. [StopLossPrice: {0}]", StopLossPrice));
+                errMessages.Add(String.Format("WARNING: StopLossPrice must be greater than 0. [StopLossPrice: {0}]", StopLossPrice));
             }
             
-            if (RiskPerTradeInUsd < 0)
+            if (RiskPerTradeInUsd <= 0)
             {
-                 err_messages.Add(String.Format("WARNING: RiskPerTradeInUsd must be greater than 0. [RiskPerTradeInUsd: {0}]", RiskPerTradeInUsd));
+                 errMessages.Add(String.Format("WARNING: RiskPerTradeInUsd must be greater than 0. [RiskPerTradeInUsd: {0}]", RiskPerTradeInUsd));
             }
             
             if (PlaceTradeDelayInMinutes < 0)
             {
-                err_messages.Add(String.Format("WARNING: PlaceTradeDelayInMinutes must be greater than 0. [PlaceTradeDelayInMinutes: {0}]", PlaceTradeDelayInMinutes));
+                errMessages.Add(String.Format("WARNING: PlaceTradeDelayInMinutes must be greater than 0. [PlaceTradeDelayInMinutes: {0}]", PlaceTradeDelayInMinutes));
             }
             
-            if (MaxAllowedOpenTrades < 0)
+            if (MaxAllowedOpenTrades <= 0)
             {
-                err_messages.Add(String.Format("WARNING: MaxAllowedOpenTrades must be greater than 0. [MaxAllowedOpenTrades: {0}]", PlaceTradeDelayInMinutes));
+                errMessages.Add(String.Format("WARNING: MaxAllowedOpenTrades must be greater than 0. [MaxAllowedOpenTrades: {0}]", PlaceTradeDelayInMinutes));
             }
             
             if (Direction == TradeDirectionType.LONG && EntryPrice < StopLossPrice)
             {
-                err_messages.Add(String.Format("WARNING: EntryPrice must be greater than stopLossPrice for LONG direction. [EntryPrice: {0}, StopLossPrice{1}]", EntryPrice, StopLossPrice));
+                errMessages.Add(String.Format("WARNING: EntryPrice must be greater than stopLossPrice for LONG direction. [EntryPrice: {0}, StopLossPrice{1}]", EntryPrice, StopLossPrice));
             }
             
             if (Direction == TradeDirectionType.SHORT && EntryPrice > StopLossPrice)
             {
-                err_messages.Add(String.Format("WARNING: EntryPrice must be lower than stopLossPrice for SHORT direction. [EntryPrice: {0}, StopLossPrice{1}]", EntryPrice, StopLossPrice));
+                errMessages.Add(String.Format("WARNING: EntryPrice must be lower than stopLossPrice for SHORT direction. [EntryPrice: {0}, StopLossPrice{1}]", EntryPrice, StopLossPrice));
             }
             
+            if (ExpirationDateString != String.Empty && !ExpirationDatePattern.IsMatch(ExpirationDateString))
+            {
+                errMessages.Add(String.Format("WARNING: ExpirationDateString must contains valid date in format YYYY/MM/DD example 2000/01/01: [ExpirationDateString: {0}]", ExpirationDateString));
+            }
+            
+            return errMessages;
+        }
+        
+        private ArrayList ValidateComputeValues()
+        {
+             var errMessages = new ArrayList();
+             
+             
             if (Amount < Symbol.VolumeInUnitsMin)
             {
-                err_messages.Add(String.Format("WARNING: Trade volume is less than minimum tradable amount: [Amount: {0}, MinTradableAmount: {1}]", Amount, Symbol.VolumeInUnitsMin));
+                errMessages.Add(String.Format("WARNING: Trade volume is less than minimum tradable amount: [Amount: {0}, MinTradableAmount: {1}]", Amount, Symbol.VolumeInUnitsMin));
             }
             
             if (Amount > Symbol.VolumeInUnitsMax)
             {
-                err_messages.Add(String.Format("WARNING: Trade volume is less than minimum tradable amount: [Amount: {0}, MinTradableAmount: {1}]", Amount, Symbol.VolumeInUnitsMin));
+                errMessages.Add(String.Format("WARNING: Trade volume is greater than maximum tradable amount: [Amount: {0}, MaxTradableAmount: {1}]", Amount, Symbol.VolumeInUnitsMax));
             }
-            
-            return err_messages;
+             
+             return errMessages;
         }
     }
 }
