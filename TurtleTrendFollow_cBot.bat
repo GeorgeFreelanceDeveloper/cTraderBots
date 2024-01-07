698e5d5e74f0c8b8f002c621e571ed996c1dfc19
LucyFreelanceDeveloper <115091833+LucyFreelanceDeveloper@users.noreply.github.com>
2024-01-07 18:25:24 +0000

    TurtleTrendFollow_cBot: add missing variable CountPeriodForEntry2 and CountPeriodForStop1

--- TurtleTrendFollow_cBot.bat
+++ TurtleTrendFollow_cBot.bat
@@ -7,13 +7,14 @@ set period="m1"
 
 ::Environment-variables
 set CountPeriodForEntry1=20
+set CountPeriodForEntry2=55
 set CountPeriodForStop1=10
 set CountPeriodForStop2=20
 set RiskPercentage=2.5
 set LongOnly=True
 
 ::ctrader bot
-ctrader-cli.exe run C:\Users\Administrator\Documents\cAlgo\Sources\Robots\TurtleTrendFollow_cBot.algo --ctid=%ctid% --pwd-file=%pwd-file% --account=%account% --symbol=%symbol% --period=%period% --environment-variables --full-acces --CountPeriodForEntry1=%CountPeriodForEntry1% --CountPeriodForEntry2=%CountPeriodForEntry2% --CountPeriodForStop2=%CountPeriodForStop2% --RiskPercentage=%RiskPercentage% --LongOnly=%LongOnly%
+ctrader-cli.exe run C:\Users\Administrator\Documents\cAlgo\Sources\Robots\TurtleTrendFollow_cBot.algo --ctid=%ctid% --pwd-file=%pwd-file% --account=%account% --symbol=%symbol% --period=%period% --environment-variables --full-acces --CountPeriodForEntry1=%CountPeriodForEntry1% --CountPeriodForEntry2=%CountPeriodForEntry2% --CountPeriodForStop1=%CountPeriodForStop1% --CountPeriodForStop2=%CountPeriodForStop2% --RiskPercentage=%RiskPercentage% --LongOnly=%LongOnly%
 
 
 
