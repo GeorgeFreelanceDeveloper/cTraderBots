d1a4ccb14e335cdb4aad4ed45b63a330d049a856
George Freelance Developer <112611533+GeorgeFreelanceDeveloper@users.noreply.github.com>
2024-01-13 18:08:10 +0000

    TurtleTrendFollow_cBot: add support enable/disable L1 and L2 levels by user
    - update README.md
    - update Description in comment

--- scripts/TurtleTrendFollow_cBot.bat
+++ scripts/TurtleTrendFollow_cBot.bat
@@ -10,11 +10,13 @@ set CountPeriodForEntry1=20
 set CountPeriodForEntry2=55
 set CountPeriodForStop1=10
 set CountPeriodForStop2=20
+set EnableL1=True
+set EnableL2=False
 set RiskPercentage=2.5
 set LongOnly=True
 set Trade1_Id=-1
 set Trade2_Id=-1
 
 ::ctrader bot
-ctrader-cli.exe run C:\Users\Administrator\Documents\cAlgo\Sources\Robots\TurtleTrendFollow_cBot.algo --ctid=%ctid% --pwd-file=%pwd-file% --account=%account% --symbol=%symbol% --period=%period% --environment-variables --full-access --CountPeriodForEntry1=%CountPeriodForEntry1% --CountPeriodForEntry2=%CountPeriodForEntry2% --CountPeriodForStop1=%CountPeriodForStop1% --CountPeriodForStop2=%CountPeriodForStop2% --RiskPercentage=%RiskPercentage% --LongOnly=%LongOnly% --Trade1_Id=%Trade1_Id% --Trade2_Id=%Trade2_Id%
+ctrader-cli.exe run C:\Users\Administrator\Documents\cAlgo\Sources\Robots\TurtleTrendFollow_cBot.algo --ctid=%ctid% --pwd-file=%pwd-file% --account=%account% --symbol=%symbol% --period=%period% --environment-variables --full-access --CountPeriodForEntry1=%CountPeriodForEntry1% --CountPeriodForEntry2=%CountPeriodForEntry2% --CountPeriodForStop1=%CountPeriodForStop1% --CountPeriodForStop2=%CountPeriodForStop2% --EnableL1=%EnableL1% --EnableL2=%EnableL2% --RiskPercentage=%RiskPercentage% --LongOnly=%LongOnly% --Trade1_Id=%Trade1_Id% --Trade2_Id=%Trade2_Id%
 
