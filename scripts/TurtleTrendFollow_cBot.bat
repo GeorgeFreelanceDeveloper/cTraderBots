195e825dc5711d9d94e625d767ea3fc4d78d87aa
LucyFreelanceDeveloper <115091833+LucyFreelanceDeveloper@users.noreply.github.com>
2024-01-28 17:42:27 +0000

    TurtleTrendFollow_cBot.bat: delete unused properties

--- scripts/TurtleTrendFollow_cBot.bat
+++ scripts/TurtleTrendFollow_cBot.bat
@@ -14,9 +14,6 @@ set EnableL1=True
 set EnableL2=False
 set RiskPercentage=2.5
 set LongOnly=True
-set Trade1_Id=-1
-set Trade2_Id=-1
 
 ::ctrader bot
-ctrader-cli.exe run C:\Users\Administrator\Documents\cAlgo\Sources\Robots\TurtleTrendFollow_cBot.algo --ctid=%ctid% --pwd-file=%pwd-file% --account=%account% --symbol=%symbol% --period=%period% --environment-variables --full-access --CountPeriodForEntry1=%CountPeriodForEntry1% --CountPeriodForEntry2=%CountPeriodForEntry2% --CountPeriodForStop1=%CountPeriodForStop1% --CountPeriodForStop2=%CountPeriodForStop2% --EnableL1=%EnableL1% --EnableL2=%EnableL2% --RiskPercentage=%RiskPercentage% --LongOnly=%LongOnly% --Trade1_Id=%Trade1_Id% --Trade2_Id=%Trade2_Id%
-
+ctrader-cli.exe run C:\Users\Administrator\Documents\cAlgo\Sources\Robots\TurtleTrendFollow_cBot.algo --ctid=%ctid% --pwd-file=%pwd-file% --account=%account% --symbol=%symbol% --period=%period% --environment-variables --full-access --CountPeriodForEntry1=%CountPeriodForEntry1% --CountPeriodForEntry2=%CountPeriodForEntry2% --CountPeriodForStop1=%CountPeriodForStop1% --CountPeriodForStop2=%CountPeriodForStop2% --EnableL1=%EnableL1% --EnableL2=%EnableL2% --RiskPercentage=%RiskPercentage% --LongOnly=%LongOnly%
